55e2dd5afc838cec2eae9286902a865cc8522e27
kungfooman <lama12345@gmail.com>
2021-09-13 11:09:12 +0200

    Work out more Multiple Dispatch style NLA Python API to work with complex NLA scenes

--- src_python/nla.py
+++ src_python/nla.py
@@ -8,37 +8,6 @@ for obj in bpy.context.scene.object:
             track.strips.new(action.name, action.frame_range[0], action)
             obj.animation_data.action = None
 			
-			
-			
-for obj in bpy.context.view_layer.objects:
-    if obj.animation_data is not None:
-	    print(obj.animation_data)
-		
-<bpy_struct, AnimData at 0x0000019DC9E2D2C8>
-<bpy_struct, AnimData at 0x0000019DC9E2D648>
-<bpy_struct, AnimData at 0x0000019DC9E2D148>
-<bpy_struct, AnimData at 0x0000019DC9E2DD48>
-
-
-for obj in bpy.context.view_layer.objects:
-    if obj.animation_data is not None:
-	    print(obj.animation_data.action)
-
-<bpy_struct, Action("Action.002") at 0x0000019DCC457428>
-<bpy_struct, Action("Action.005") at 0x0000019DCC456E88>
-<bpy_struct, Action("Action.008") at 0x0000019DCC459DC8>
-None
-
-
-for obj in bpy.context.view_layer.objects:
-    if obj.animation_data is not None:
-	    print(obj.animation_data.nla_tracks)
-		
-<bpy_collection[1], NlaTracks>
-<bpy_collection[1], NlaTracks>
-<bpy_collection[2], NlaTracks>
-<bpy_collection[1], NlaTracks>
-
 
 # arr = Array([1,10,2,20,3,30])
 # arr.push(4, 40)
@@ -47,6 +16,9 @@ for obj in bpy.context.view_layer.objects:
 class Array(list):
     def map(self, cb):
         return Array(map(cb, self));
+    def forEach(self, cb):
+        for _ in self:
+            cb(_);
     def filter(self, cb):
         return Array(filter(cb, self));
     def filtermap(self, cb):
@@ -57,41 +29,123 @@ class Array(list):
         for arg in args:
             self.append(arg);
 
+#  getObjects().map(getName)                          # == ['Light', 'Camera', 'Bip001', 'Maila_EyeL', ...
+def getObjects():
+    return Array(bpy.context.view_layer.objects);
 
+# getAnimationDatas().map(getInfo)
 def getAnimationDatas():
-    animationDatas = Array();
-    for obj in bpy.context.view_layer.objects:
-        animation_data = obj.animation_data
-        if obj.animation_data is None:
-            continue;
-        animationDatas.push(animation_data);
-    return animationDatas;
+    return getObjects().filtermap(lambda object: object.animation_data);
 
+def getAction(_):
+    if type(_) == bpy.types.Object:
+        if _.animation_data is not None:
+            return _.animation_data.action;
+    elif type(_) == bpy.types.AnimData:
+        return _.action;
+
+# getMarkers().map(getName)                           # == ['F_42', 'A', 'C', 'B']
+def getMarkers():
+    return Array(bpy.context.scene.timeline_markers)
+
+# b = getMarker('B')
+def getMarker(name):
+    return bpy.context.scene.timeline_markers.get(name);
+
+def createMarker(name, frame=None):
+    if frame is None:
+        frame = bpy.context.scene.frame_current;
+    bpy.context.scene.timeline_markers.new(name, frame=frame)
+
+def select(_):
+    _.select = True;
+
+def deselect(_):
+    _.select = False;
+
+# getActions().map(getName)                           # == ['Action.002', 'Action.005', 'Action.008']
+# getActions().map(getInfo)
 def getActions():
-    getAnimationDatas().filtermap(lambda x: x.action)
+    return getAnimationDatas().filtermap(lambda x: x.action)
 
-    actions = Array();
-    for obj in bpy.context.view_layer.objects:
-        animation_data = obj.animation_data
-        if obj.animation_data is None:
-            continue;
-        action = animation_data.action
-        if action is None:
-            continue;
-        actions.push(action);
-    return actions;
+# flat([[1, 2], [3, 4]])                              # == [1, 2, 3, 4]
+def flat(_):
+    return Array([x for __ in _ for x in __]);
 
-def info(_):
-    if (type(_) == bpy.types.Action):
-        infoAction(_);
+# getNlaTracksArray()[2][0].name                      # == '[Action Stash]'
+# getNlaTracksArray()[2][1].name                      # == 'LeftEyeTrack'
+# getNlaTracksArray().map(lambda tracks: len(tracks)) # == [1, 1, 2, 1]
+# getNlaTracksArray().map(len)                        # == [1, 1, 2, 1]
+# getNlaTracksArray().map(getNames)                   # == [['NlaTrack'], ['RightEyeTrack'], ['[Action Stash]', 'LeftEyeTrack'], ['EyeTargetTrack']]
+def getNlaTracksArray():
+    return getAnimationDatas().filtermap(lambda x: Array(x.nla_tracks))
+
+# getNlaTracks().map(getName)                         # == ['NlaTrack', 'RightEyeTrack', '[Action Stash]', 'LeftEyeTrack', 'EyeTargetTrack']
+def getNlaTracks(o=None):
+    if o is not None:
+        return o.animation_data.nla_tracks;
     else:
-        print('unknown thing', type(_), _);
+        return flat(getNlaTracksArray());
 
-def infoAction(action):
-    print("action %s = {users: %d" % (action.name, action.users));
-  
+def getName(_):
+    if _ is None:
+        return None;
+    if not hasattr(_, 'name'):
+        return None;
+    return _.name
+
+def getNames(_):
+    return _.map(getName);
+
+# getActions().map(getInfo);
+def getInfo(_):
+    t = type(_)
+    if t == bpy.types.Action:
+        print("Action {name='%s' users: %d, fcurves: #%d, frame_range=%s, pose_markers=#%d}" % (
+            _.name, _.users, len(_.fcurves), _.frame_range, len(_.pose_markers)
+        ));
+    elif t == bpy.types.TimelineMarker:
+        print("TimelineMarker {name='%s' frame=%d, select=%s, camera=%s}" % (
+            _.name, _.frame, _.select, _.camera
+        ));
+    elif t == bpy.types.NlaTrack:
+        print("NlaTrack {name='%s' active=%s, select=%s, strips=#%d, mute=%s}" % (
+            _.name, _.active, _.select, len(_.strips), _.mute
+        ));
+    else:
+        print('unknown thing', t, _);
+        return False;
+    return True;
+
+def getFirst(array):
+    return array[0];
+
+def getLast(array):
+    return array[len(array) - 1];
+
+def newTrack(obj, name=None):
+    _ = obj.animation_data.nla_tracks;
+    prev = getLast(_); # will be shown at top in NLA Editor
+    #prev = getFirst(_);
+    track = _.new(prev=prev)
+    if name is not None:
+        track.name = name;
+    return track;
 
 getAnimationDatas().filtermap(lambda x: x.action)
 
 actions = getActions();
 a, b, c, d = getActions();
+
+o = getObjects().filter(lambda o: o.name == 'EyeLeftParent')[0]
+newTrackA = o.animation_data.nla_tracks.new()
+
+# >>> bpy.types.NlaStrips()
+# Traceback (most recent call last):
+#   File "<blender_console>", line 1, in <module>
+# TypeError: bpy_struct.__new__(type): expected a single argument
+
+# >>> prev=o.animation_data.nla_tracks[0]
+# >>> o.animation_data.nla_tracks.new(prev=prev)
+# bpy.data.objects['EyeLeftParent']...NlaTrack
+
