11debe830d9009992efb773a5d95b7bd78ab6bfe
kungfooman <lama12345@gmail.com>
2021-09-21 16:57:53 +0200

    dumpVerlet: add pins

--- src_duktape/dumpVerlet.ts
+++ src_duktape/dumpVerlet.ts
@@ -6,13 +6,15 @@ import { selectedObject } from "./selectedObjects";
  *   console.clear();
  *   reload();
  *   var mesh = selectedObject().mesh;
- *   out = dumpVerlet(mesh);
+ *   // select in edit mode, go to object mode, otherwise vertices are not selected for some reason
+ *   pins = mesh.vertices.filter(v=>v.selected).map(v=>v.i);
+ *   out = dumpVerlet(mesh, pins);
  *   console.log(out);
  * ```
  * @returns 
  */
 
-export function dumpVerlet(mesh: Mesh) {
+export function dumpVerlet(mesh: Mesh, pins: number[]) {
   var out = '{\n';
   out += '  "kind": "verlet",\n';
   out += `  "name": "${mesh.name}",\n`;
@@ -21,7 +23,8 @@ export function dumpVerlet(mesh: Mesh) {
   out += '  ],\n';
   out += '  "edges": [\n';
   out += mesh.edges.map(edge=>`    [${edge.v1}, ${edge.v2}]`).join(',\n') + '\n';
-  out += '  ]\n';
+  out += '  ],\n';
+  out += `  "pins": [${pins.join(', ')}]\n`;
   out += '}\n';
   return out;
 }
